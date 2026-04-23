e693ad22126a1433e24a15f56ab6c3c4a087ff2a
Khushbu Chauhan <88323876+ckhushabu6@users.noreply.github.com>
2026-04-23 02:38:27 +0000

    Majority Element

---
+++ Hashmap/realProblems/majorityElement.js
@@ -0,0 +1,19 @@
+function majorityElement(arr){
+    //here i have not need index so i will use "for of loop";
+    
+    let map = new Map();
+    let array = [];
+    //check frequency 
+    for(let nums of arr){
+        map.set(nums , (map.get(nums) || 0) + 1);
+        if(map.get(nums) > arr.length/2) return nums
+    }
+   return -1;
+
+   
+    
+}
+
+
+let arr = [3,3,4,2,4,4,4,3,3,3,3 ];
+console.log(majorityElement(arr));
