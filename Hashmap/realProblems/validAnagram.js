2bb630d182de827229063f7cd9b8134d6788829d
Khushbu Chauhan <88323876+ckhushabu6@users.noreply.github.com>
2026-04-23 02:07:42 +0000

    Anagram problem

--- Hashmap/realProblems/validAnagram.js
+++ Hashmap/realProblems/validAnagram.js
@@ -1,22 +1,34 @@
 function isAnagram(s , t){
-    let map = new Map();
-    for(let i =0 ; i< s.length ; i++){
-        if(map.has(s[i])){
-            map.set(s[i] . map.get(s[i])+1)
-        }else{
-            map.set(s[i] , 1);
+    //step 1 check length is equal or not if no return no ;
+    //step 2 find the frequency of s 
+    //step 3 check same character is exist or not 
+    //step 4 check frequency of character
 
-        }
-    }
+    //______________________________________________
 
-    for(let i = 0 ; i<t.length ; i++){
-        if(map.has(t[i])){
-            
-        }
+    //step 1
+    if(s.length !== t.length) return false;
+    let map = new Map ;
+    //step2
+    // for(let i = 0 ; i<s.length ; i++){
+    //     if(map.has(s[i])){
+    //         map.set(s[i] , map.get(s[i])+1);
+    //     }else{
+    //         map.set(s[i], 1);
+    //     }
+    // }
+    for(let char of s){
+        map.set(char , (map.get(char) || 0) +1);
     }
-    return map;
+    //step 3
+    for(let char of t){
+        if(!map.has(char)) return false ;
+        map.set(char , (map.get(char)||0) - 1);
+        if(map.get(char) < 0) return false;
+    }
+return true;
 }
 
 
-let s = "listen", t = "silent"
+let s = "lstten", t = "sitlent"
 console.log(isAnagram(s , t));
